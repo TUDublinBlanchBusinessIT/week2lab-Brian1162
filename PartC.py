f7ad59a2927799c55f1a4f11b1d69d7eb0e4c9cc
Brian Watters <Brian.Watters@itb.ie>
2024-01-30 13:09:03 +0000

    PartC.py completted

---
+++ PartC.py
@@ -0,0 +1,12 @@
+#Brian Watters
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen.  The printSteps function takes
+#2 values, which control the number of iterations it runs.
+
+def  printSteps(a,b):
+    for i in range(a, b+1):
+        print(f"Step {i}")
+    
+
+printSteps(1,9)
+
